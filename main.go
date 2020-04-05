ea12fa64dfa182a1419ca9e28b362a1fec5b38fd
araquach <adam@contra-design.com>
2020-04-05 14:02:20 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,269 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	tplIndex *template.Template
+)
+
+type Error struct {
+	Message string `json:"message"`
+}
+
+type User struct {
+	ID        	uint       	`json:"id" gorm:"primary_key"`
+	Name 		string 		`json:"name"`
+	Email 		string 		`json:"email" gorm:"unique_index"`
+	Password 	string 		`json:"password"`
+	IsAdmin		bool 		`json:"is_admin"`
+	Token		string 		`json:"token" gorm:"-"`
+}
+
+type Test struct {
+	Id			uint 	`json:"id" gorm: "primary_key"`
+	FirstName 	string 	`json:"first_name"`
+	LastName	string 	`json:"last_name"`
+}
+
+func dbConn() (db *gorm.DB) {
+	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
+
+func main() {
+	var err error
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	db := dbConn()
+	db.LogMode(true)
+	db.AutoMigrate(&User{}, &Test{})
+	db.Close()
+
+	tplIndex = template.Must(template.ParseFiles(
+		"views/layouts/main.gohtml",
+		"views/pages/index.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc(`/{[a-zA-Z0-9=\-\/]+}`, index).Methods("GET")
+
+	// api
+	r.HandleFunc("/api/register", apiRegister).Methods("POST")
+	r.HandleFunc("/api/login", apiLogin).Methods("POST")
+	r.HandleFunc("/api/test", TokenVerifyMiddleWare(apiTest)).Methods("GET")
+
+	// Styles
+	assetHandler := http.FileServer(http.Dir("./dist/"))
+	assetHandler = http.StripPrefix("/dist/", assetHandler)
+	r.PathPrefix("/dist/").Handler(assetHandler)
+
+	log.Printf("Starting server on %s", port)
+
+	http.ListenAndServe(":" + port, r)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if err := tplIndex.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func apiRegister(w http.ResponseWriter, r *http.Request) {
+	var user User
+	var error Error
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" {
+		error.Message = "Email is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	if user.Password == "" {
+		error.Message = "Password is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user.Password = string(hash)
+
+	db := dbConn()
+	db.Create(&user)
+
+	if err != nil {
+		error.Message = "Server error."
+		respondWithError(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	user.Password = ""
+
+	w.Header().Set("Content-Type", "application/json")
+	responseJSON(w, user)
+}
+
+func apiLogin(w http.ResponseWriter, r *http.Request) {
+	var user User
+	var error Error
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	if user.Email == "" {
+		error.Message = "Email is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	if user.Password == "" {
+		error.Message = "Password is missing."
+		respondWithError(w, http.StatusBadRequest, error)
+		return
+	}
+
+	password := user.Password
+
+	db := dbConn()
+	db.Where("email = ?", user.Email).First(&user)
+
+	hashedPassword := user.Password
+
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+
+	if err != nil {
+		error.Message = "Invalid Password"
+		respondWithError(w, http.StatusUnauthorized, error)
+		return
+	}
+
+	token, err := GenerateToken(user)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	user.Token = token
+	user.Password = ""
+
+	w.Header().Set("Content-Type", "application/json")
+	responseJSON(w, user)
+}
+
+func apiTest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	db := dbConn()
+	test := []Test{}
+	db.Find(&test)
+	db.Close()
+
+	json, err := json.Marshal(test)
+	if err != nil {
+		log.Println(err)
+	}
+	w.Write(json)
+}
+
+func respondWithError(w http.ResponseWriter, status int, error Error) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(error)
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+}
+
+func GenerateToken(user User) (string, error) {
+	var err error
+	secret := "secret"
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"iss":   "course",
+	})
+
+	tokenString, err := token.SignedString([]byte(secret))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return tokenString, nil
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var errorObject Error
+		authHeader := r.Header.Get("Authorization")
+		bearerToken := strings.Split(authHeader, " ")
+
+		if len(bearerToken) == 2 {
+			authToken := bearerToken[1]
+
+			token, error := jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+
+				return []byte("secret"), nil
+			})
+
+			if error != nil {
+				errorObject.Message = error.Error()
+				respondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+
+			if token.Valid {
+				next.ServeHTTP(w, r)
+			} else {
+				errorObject.Message = error.Error()
+				respondWithError(w, http.StatusUnauthorized, errorObject)
+				return
+			}
+		} else {
+			errorObject.Message = "Invalid token."
+			respondWithError(w, http.StatusUnauthorized, errorObject)
+			return
+		}
+	})
+}
